8f5b3eb63605fcb0e12762ab608d9243199060ed
Mahdiyeh <101658092+mahdiyeh-asgharpour@users.noreply.github.com>
2022-12-21 16:19:57 +0330

    Add files via upload

--- js.js
+++ js.js
@@ -11,9 +11,33 @@ if (answer == true) {
 
 
 
-    }
 
-} else
+    }
+    const white1 = document.createElement("h1");
+    document.getElementById("3-4").appendChild(white1);
+    white1.id = "h3-4";
+    white1.style.backgroundColor = "white";
+    white1.innerText = "o ";
+    white1.style.color = "white";
+    const white2 = document.createElement("h1");
+    document.getElementById("4-3").appendChild(white2);
+    white2.id = "h4-3";
+    white2.style.backgroundColor = "white";
+    white2.innerText = "o ";
+    white2.style.color = "white";
+
+    const black1 = document.createElement("h1");
+    document.getElementById("3-3").appendChild(black1);
+    black1.id = "h3-3";
+    black1.style.backgroundColor = "black";
+    black1.innerText = "o";
+    const black2 = document.createElement("h1");
+    document.getElementById("4-4").appendChild(black2);
+    black2.id = "h4-4";
+    black2.style.backgroundColor = "black";
+    black2.innerText = "o ";
+
+} else {
     for (let i = 0; i < 8; i++) {
         document.getElementById("g1").innerHTML += `<span id='1-${Number(i+1)}'></span>`;
         document.getElementById("g2").innerHTML += `<span id='2-${Number(i+1)}'></span>`;
@@ -25,4 +49,29 @@ if (answer == true) {
         document.getElementById("g8").innerHTML += `<span id='8-${Number(i+1)}'></span>`;
 
 
+
     }
+    const white1 = document.createElement("h1");
+    document.getElementById("4-5").appendChild(white1);
+    white1.id = "h4-5";
+    white1.style.backgroundColor = "white";
+    white1.innerText = "o ";
+    white1.style.color = "white";
+    const white2 = document.createElement("h1");
+    document.getElementById("5-4").appendChild(white2);
+    white2.id = "h5-4";
+    white2.style.backgroundColor = "white";
+    white2.innerText = "o ";
+    white2.style.color = "white";
+
+    const black1 = document.createElement("h1");
+    document.getElementById("4-4").appendChild(black1);
+    black1.id = "h4-4";
+    black1.style.backgroundColor = "black";
+    black1.innerText = "o";
+    const black2 = document.createElement("h1");
+    document.getElementById("5-5").appendChild(black2);
+    black2.id = "h5-5";
+    black2.style.backgroundColor = "black";
+    black2.innerText = "o ";
+}
