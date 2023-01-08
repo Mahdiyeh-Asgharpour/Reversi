784b72423cfd095428dd18612f665eb96015af0f
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2023-01-08 12:12:46 +0330

    Add files via upload

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
 const answer = confirm("6x6 or 8x8?if u choose 6*6 click ok plz.");
+var white=[];
+var black=[];
 if (answer == true) {
     for (let i = 0; i < 6; i++) {
 
@@ -19,12 +21,16 @@ if (answer == true) {
     white1.style.backgroundColor = "white";
     white1.innerText = "o ";
     white1.style.color = "white";
+    white.push(white1);
+
+
     const white2 = document.createElement("h1");
     document.getElementById("4-3").appendChild(white2);
     white2.id = "h4-3";
     white2.style.backgroundColor = "white";
     white2.innerText = "o ";
     white2.style.color = "white";
+    white.push(white2);
 
     const black1 = document.createElement("h1");
     document.getElementById("3-3").appendChild(black1);
@@ -32,6 +38,8 @@ if (answer == true) {
     black1.style.backgroundColor = "black";
     black1.innerText = "o";
     black1.style.color = "black";
+    black.push(black1);
+
 
     const black2 = document.createElement("h1");
     document.getElementById("4-4").appendChild(black2);
@@ -39,6 +47,8 @@ if (answer == true) {
     black2.style.backgroundColor = "black";
     black2.innerText = "o ";
     black2.style.color = "black";
+    black.push(black2);
+
 
 
 } else {
@@ -61,12 +71,20 @@ if (answer == true) {
     white1.style.backgroundColor = "white";
     white1.innerText = "o ";
     white1.style.color = "white";
+    white.push(white1);
+
+
+
     const white2 = document.createElement("h1");
     document.getElementById("5-4").appendChild(white2);
     white2.id = "h5-4";
     white2.style.backgroundColor = "white";
     white2.innerText = "o ";
     white2.style.color = "white";
+    white.push(white2);
+
+
+
 
     const black1 = document.createElement("h1");
     document.getElementById("4-4").appendChild(black1);
@@ -74,6 +92,9 @@ if (answer == true) {
     black1.style.backgroundColor = "black";
     black1.innerText = "o";
     black1.style.color = "black";
+    black.push(black1);
+
+
 
     const black2 = document.createElement("h1");
     document.getElementById("5-5").appendChild(black2);
@@ -81,6 +102,8 @@ if (answer == true) {
     black2.style.backgroundColor = "black";
     black2.innerText = "o ";
     black2.style.color = "black";
+    black.push(black2);
+
 
 }
 var index = 0;
@@ -92,20 +115,22 @@ function clicki(id) {
     } else {
 
         if (Number(index) % 2 === 0) {
-            const white = document.createElement("h1");
-            document.getElementById(id).appendChild(white);
-            white.id = "h" + id;
-            white.style.backgroundColor = "white";
-            white.innerText = "o ";
-            white.style.color = "white";
+            const whitee = document.createElement("h1");
+            document.getElementById(id).appendChild(whitee);
+            whitee.id = "h" + id;
+            whitee.style.backgroundColor = "white";
+            whitee.innerText = "o ";
+            whitee.style.color = "white";
+            white.push(whitee);
             index++;
         } else {
-            const black = document.createElement("h1");
-            document.getElementById(id).appendChild(black);
-            black.id = "h" + id;
-            black.style.backgroundColor = "black";
-            black.innerText = "o ";
-            black.style.color = "black";
+            const blackk = document.createElement("h1");
+            document.getElementById(id).appendChild(blackk);
+            blackk.id = "h" + id;
+            blackk.style.backgroundColor = "black";
+            blackk.innerText = "o ";
+            blackk.style.color = "black";
+            black.push(blackk);
             index++;
 
         }
@@ -118,14 +143,43 @@ function reversigame(id){
     const idr=idd.id.charAt(0);
     const mohre=document.getElementById("h"+id);
     const color=mohre.style.color;
-    for(i=1;i<=idr;i++)
-        if(document.getElementById("h"+i+"-"+idc).style.color===color)
-            for(j=i+1;j<idr;j++)
-                document.getElementById("h"+j+"-"+idc).style.color=color;
-
+    if(color =="white")
+    {
+        for(let i=0;i<black.length;i++)
+        {
+            const row= black[i].id.charAt(0);
+            const col=black[i].id.charAt(2);
+            const sum=Number(col)+Number(row);
+            const mine=Number(col)-Number(row);
+            if(row==idr ||col==idc ||sum ==Number(idc+idr)||mine==Number(idc-idr))
+            {
+                black[i].id.style.color="white";
+                white.push(black[i]);
+                black.pop(black[i]);
+            }
+           
+        }
+    }
+    else{
+        for(let i=0;i<white.length;i++)
+        {
+            const row= black[i].id.charAt(0);
+            const col=black[i].id.charAt(2);
+            const sum=Number(col)+Number(row);
+            const mine=Number(col)-Number(row);
+            if(row==idr ||col==idc ||sum ==Number(idc+idr)||mine==Number(idc-idr))
+            {
+                white[i].id.style.color="black";
+                black.push(white[i]);
+                white.pop(white[i]);
+            }
+        }
+    }
 }
 
 function reversi(id) {
     clicki(id);
     reversigame(id);
+    console.log(white);
+    console.log(black);
 }
